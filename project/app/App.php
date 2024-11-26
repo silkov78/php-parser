959a6db810ef795fb233bdaeceb00de5ed1ce2cb
silkov78 <piotr.silkov78@gmail.com>
2024-11-26 10:22:00 +0300

    getTransactionFiles func

--- project/app/App.php
+++ project/app/App.php
@@ -1,54 +1,20 @@
 <?php
 
-function getCsvPathes(string $sourceDir): array {
-  $resultArray = [];
-  $sourceList = scandir($sourceDir);
+declare(strict_types=1);
 
-  foreach ($sourceList as $file) {
-    if (str_ends_with($file, '.csv')) {
-      array_push($resultArray, $sourceDir . '/' . $file);
+// Code
+function getTransactionFiles(): array {
+  
+  $files = [];
+
+  foreach(scandir(FILES_PATH) as $file){
+    if (is_dir($file)) {
+      continue;
     }
+
+    $files[] = $file;
   }
 
-  return $resultArray;
+  return $files;
 }
 
-function getTransactionsFromFile(string $filePath): array {
-  $file = fopen($filePath, 'r');
-  $headers = fgetcsv($file);
- 
-  $resultArray = [];
-  while(($row = fgetcsv($file)) !== false) {
-      $tempRow = array_combine(keys: $headers, values: $row);
-      $tempRow['Amount_ed'] = str_replace('$', '', $tempRow['Amount']);
-      $tempRow['Amount_ed'] = str_replace(',', '', $tempRow['Amount_ed']);
-      $tempRow['Amount_ed'] = (float) $tempRow['Amount_ed'];
-      $resultArray[] = $tempRow; 
-  }
-
-  fclose($file);
-  return $resultArray;
-}
-
-// Execution
-
-define("TRANSACTIONS_PATH", "../transactions");
-$csvTransactionsFiles = getCsvPathes(TRANSACTIONS_PATH);
-
-$transactionsArray = [];
-foreach ($csvTransactionsFiles as $filePath) {
-  $tempArray = getTransactionsFromFile($filePath);
-  $transactionsArray = array_merge($transactionsArray, $tempArray);
-}
-
-$transationsIncome = 0;
-$transactionsExpense = 0;
-
-foreach($transactionsArray as $value) {
-   $amount = $value['Amount_ed'];
-   if ($amount >= 0) {
-        $transactionsIncome += $amount;
-   } else {
-        $transactionsExpense += $amount;
-   }
-}
--- project/public/index.php
+++ project/public/index.php
@@ -7,3 +7,8 @@ $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+// Execution
+require APP_PATH . 'App.php';
+
+var_dump(getTransactionFiles());
