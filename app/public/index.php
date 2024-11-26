9f8c4b4ec1714e7e266118edd161054035160640
silkov78 <piotr.silkov78@gmail.com>
2024-11-26 09:02:16 +0300

    define project structure in public/index.php

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,9 @@
 <?php
 
-require '../views/transactions.php';
+declare(strict_types = 1);
+
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+define('MODELS_PATH', $root . 'models' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'models' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $root . 'models' . DIRECTORY_SEPARATOR);
