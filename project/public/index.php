b9618f79fbdfcd6c5e4702a5bc1a0b20f0eb1ce9
silkov78 <piotr.silkov78@gmail.com>
2024-11-26 09:05:16 +0300

    new folder pathes

--- project/public/index.php
+++ project/public/index.php
@@ -4,6 +4,6 @@ declare(strict_types = 1);
 
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
-define('MODELS_PATH', $root . 'models' . DIRECTORY_SEPARATOR);
-define('FILES_PATH', $root . 'models' . DIRECTORY_SEPARATOR);
-define('VIEWS_PATH', $root . 'models' . DIRECTORY_SEPARATOR);
+define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transactions' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
