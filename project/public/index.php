04b52269cd1ec9924a2717ed90f7e7d32c07ae41
silkov78 <piotr.silkov78@gmail.com>
2024-11-26 09:59:40 +0300

    transactions to transaction_files

--- project/public/index.php
+++ project/public/index.php
@@ -5,5 +5,5 @@ declare(strict_types = 1);
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
 define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
-define('FILES_PATH', $root . 'transactions' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
